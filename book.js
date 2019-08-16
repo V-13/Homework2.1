375b17a072ec9af09177731e8d738246803faf2a
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-16 23:36:35 +0530

    homework books

--- book.js
+++ book.js
@@ -1,11 +1,80 @@
 const Exp = require("express")
 var app1 = new Exp();
 app1.set('view engine','ejs');
+ name =[
+    {
+      title:"War and peace",
+      genre:"history",
+      author:"Leo Tolstoy",
+      DOP:"1867" 
+},
+
+{
+    title:"Harry potter",
+    genre:"fiction",
+    author:"JK Rowling",
+    DOP:"1997"
+},
+
+{
+    title:"Wings of fire",
+    genre:"autobiography",
+    author:"APJ Abdul Kalam",
+    DOP:"1999"
+},
+
+{
+    title:"Lord of rings",
+    genre:"Adventure",
+    author:"JRR Tolkien",
+    DOP:"1954"
+},
+{
+    title:"Oliver Twist",
+    genre:"Childrens novel",
+    author:"Charles Dickens",
+    DOP:"1839"
+},
+{
+    title:"Angels & Demons",
+    genre:"Sci-Fi",
+    author:"Dan brown",
+    DOP:"2000"
+},
+{
+    title:"The Bourne Identity",
+    genre:"Action",
+    author:"Robert Ludlum",
+    DOP:"1980"
+},
+{
+    title:"The Alchemist",
+    genre:"Inspiration",
+    author:"Paulo Coelho",
+    DOP:"1988"
+},
+{
+    title:"A Brief History of Time",
+    genre:"Science",
+    author:"Stephen Hawking",
+    DOP:"1988"
+},
+{
+    title:"The Kite Runner",
+    genre:"fiction",
+    author:"Khaled Hosseini",
+    DOP:"2003"
+},
+
+
+
+
+];
 app1.get('/',(rq,rs)=>{
-    rs.render("add")
+    rs.render("add");
 });
 app1.get('/view',(rq,rs)=>{
-    rs.render("view",{"names":["Wings of fire","Hobbit","Digital fortess","Motor cycle diaries","Harry potter","Oliwer Twist","War and Peace","Sherlock Holmes","Gulliver's Travels","Alchemist"]})
+    rs.render("view",name);
 });
 app1.listen(process.env.PORT|| 3003,()=>{
     console.log("server running")
